22b37b6daa1ae7b941276ad5676222c599c567e7
N1C0N1C0 <wech7883@naver.com>
2025-04-13 12:45:06 +0900

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Headerにて、ページをスクロールする時、ダークのスタイルが適用
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if(window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  };
+});
