5a13e49602da96097572de2ea24040dfc606255b
N1C0N1C0 <wech7883@naver.com>
2025-04-13 23:29:03 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-// Headerにて、ページをスクロールする時、ダークのスタイルが適用
+// Headerにて、ページをスクロールする時、ダークのスタイルが適用する。
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
   if(window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   };
+});
+
+// Homeにて、ページをスクロールすると、徐々にフェードアウトさせるようにする。
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
