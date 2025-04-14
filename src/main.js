1938522f596593fe2e5d473cffbd100b1ed2a708
N1C0N1C0 <wech7883@naver.com>
2025-04-15 00:47:51 +0900

    JS-Navbar

--- src/main.js
+++ src/main.js
@@ -12,7 +12,28 @@ document.addEventListener('scroll', () => {
 // Homeにて、ページをスクロールすると、徐々にフェードアウトさせるようにする。
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
-
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Arrowボタンの制御を調整する。
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.display = 'block';
+  } else {
+    arrowUp.style.display = 'none';
+  }
+});
+
+// Navbar トグルボタンクリック処理
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Navbar メニュークリックする時、メニューを自動的に閉じる
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+})
